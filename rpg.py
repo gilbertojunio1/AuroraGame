e3efd83fae9fdf61f33f60ea95c8383abdbd0252
Barbara Sousa <barbinhava@gmail.com>
2016-06-13 03:07:43 -0300

    Codigo(Atualizado)

--- rpg.py
+++ rpg.py
@@ -3,10 +3,18 @@ import textorpg
 textorpg.historia()
 textorpg.caminho()
 escolha = input ("Qual sua escolha?\n⩥ ")
-if escolha == "a":
+if escolha == "1":
     textorpg.castelo()
-escolha = input("Qual sua escolha?\n⩥ ")
-if escolha=="a":
-    textorpg.finalA()
-elif escolha!="a": 
-    textorpg.finalB()
+if escolha == "2":
+    textorpg.vilas()
+    escolha = input("Qual sua escolha?\n⩥ ")
+if escolha == "a" or escolha == "b":
+    textorpg.orc()
+    escolha = input("Qual sua escolha?\n⩥ ")
+if escolha == "3":
+    textorpg.castelo()
+escolha = input("Qual sua escolha?\n⩥ ")    
+if escolha == "8":
+    textorpg.final8()
+if escolha == "9": 
+    textorpg.final9()
